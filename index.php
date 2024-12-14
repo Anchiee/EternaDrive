54337f21af52f429376fd5050ccd39fc477b5e99
Anchie <wyhwtf@gmail.com>
2024-12-14 20:37:29 +0100

    file init

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Main</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="view/styles/main.css">
+  <link rel="stylesheet" href="view/styles/index.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Delius&family=Funnel+Display:wght@300..800&family=Host+Grotesk:ital,wght@0,300..800;1,300..800&family=Lexend:wght@100..900&family=Manrope:wght@200..800&family=Pacifico&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Rouge+Script&display=swap" rel="stylesheet">
+</head>
+<body>
+  
+<nav>
+    <a href="#"><strong>Eterna</strong>Drive</a>
+    
+    <div class="navigation-options">
+      <a href="#">Sign-in</a>
+      <a href="#">Log-in</a>
+      <a href="#">Contact</a>
+      <a href="#">About us</a>
+    </div>
+  </nav>
+
+  <main>
+    <section class="header-section">
+      <div class="text-container">
+        <h1>Your files, your way. Forever.</h1>
+        <p>
+          keep your files safely using <strong>Eterna</strong>Drive
+          and stop worrying about your storage.
+        </p>
+
+        <button>Get started</button>
+        
+        
+      </div>
+      <img src="assets/logo.png" alt="logo" width="400">
+  </main>
+
+  <footer>
+    <p>© 2024 Eternadrive. All Rights Reserved.<br>
+    Eternadrive is a secure, open-source file-sharing platform licensed under the MIT License.
+    Effortlessly store, share, and access your files anytime, anywhere.</p>
+
+    <p><strong>Connect with us</strong></p>
+    <address>
+      <a href="#">Privacy policy</a>
+      <a href="#">Terms of service</a>
+      <a href="#">Github repository</a>
+    </address>
+    
+  </footer>
+  
+</body>
+</html>

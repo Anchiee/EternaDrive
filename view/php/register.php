a6db20483b25f23fa9997eafa8706772fa105033
Anchie <wyhwtf@gmail.com>
2024-12-18 15:18:11 +0100

    Made frontend for registration

--- view/php/register.php
+++ view/php/register.php
@@ -9,34 +9,46 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Delius&family=Funnel+Display:wght@300..800&family=Host+Grotesk:ital,wght@0,300..800;1,300..800&family=Lexend:wght@100..900&family=Manrope:wght@200..800&family=Pacifico&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Rouge+Script&display=swap" rel="stylesheet">
-  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
 
   <main>
     
     <section class="input-section">
-        <form action="#" method="post">
-          <label for="username">Username</label>
-          <input type="text" name="username" id="username">
+      <div class="socials">
+          <h1>Use other socials</h1>
 
-          <label for="password">Password</label>
-          <input type="password" name="password" id="password">
-          
-          <label for="confirmPassword">Confirm password</label>
-          <input type="confirmPassword" name="confirmPassword" id="confirmPassword">
-          
-          <label for="email">Email</label>
-          <input type="email" name="email" id="email">
+          <address>
+            <a href="#"><i class="fa-brands fa-google fa-xl" style="color: #444444;"></i></a>
+            <a href="#"><i class="fa-brands fa-github fa-xl" style="color: #444444;"></i></i></a>
 
-          <button>Sign-in</button>
-        </form>
-      </section>
+          </address>
+      </div>
+
+      <h2>Sign-in</h2>
+      <form action="#" method="post">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" placeholder="Enter your username">
+
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" placeholder="Enter your passsword">
+        
+        <label for="confirmPassword">Confirm password</label>
+        <input type="confirmPassword" name="confirmPassword" id="confirmPassword" placeholder="Confirm your password">
+        
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" placeholder="Enter your email">
+
+        <button>Sign-in</button>
+      </form>
+    </section>
 
     <section class="info-section">
-      <h1>Sign-in</h1>
+      
+      <img src="../../assets/cloud.png" width="300" alt="cloud image">
+      <h3><strong>Your files, your way. Forever.</strong></h3>
       <p><strong>Eterna</strong>Drive is free for everyone, all it takes is a few clicks! Join to the wide community of people.</p>
-      <p><strong>Your files, your way. Forever.</strong></p>
     </section>
   
   </main>

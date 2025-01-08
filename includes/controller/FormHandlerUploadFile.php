8cb7921dbf077594695df6c9c1b878b01971c1ac
Anchie <wyhwtf@gmail.com>
2025-01-08 16:13:05 +0100

    Hotfix

--- includes/controller/FormHandlerUploadFile.php
+++ includes/controller/FormHandlerUploadFile.php
@@ -6,13 +6,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
   require_once "../model/AddData.php";
   require_once "../model/ReadData.php";
 
+  print_r($_FILES["file-upload"]);
 
   $username = $_SESSION["username"];
   $fileName = basename($_FILES["file-upload"]["name"]);
 
 
   //i didnt add '../../' because simple uploads/ would be more flexible
-  $dir = "uploads/" . $username . "/" . $fileName . "/";
+  $dir = "uploads/" . $username . "/";
   $targetDir = "../../" . $dir . $fileName;
 
   if(!is_dir("../../" . $dir)) {

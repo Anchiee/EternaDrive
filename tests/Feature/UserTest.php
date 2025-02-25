e166dc3835567aca695a7f1be8ed4e9fbe9b4360
Anchie <wyhwtf@gmail.com>
2025-02-25 20:57:40 +0100

    more user tests

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -14,6 +14,35 @@ test('users can access the page', function () {
     $response->assertStatus(200);
 });
 
+
+test("users can change the info", function() {
+    $user = User::factory()->create([
+        "password" => bcrypt("password")
+    ]);
+
+    $this->actingAs($user)->put("/settings", [
+        "name" => "updated_name",
+        "password" => "password"
+    ]);
+
+    $this->assertDatabaseHas("users", [
+        "name" => "updated_name"
+    ]);
+});
+
+test("empty input error upon giving empty input", function() {
+    $user = User::factory()->create([
+        "password" => bcrypt("password")
+    ]);
+
+    $response = $this->actingAs($user)->put("/settings", [
+        "name" => "",
+        "password" => "password"
+    ]);
+
+    $response->assertSessionHasErrors("name");
+});
+
 test("files are being deleted upon user deleting their account", function() {
     Storage::fake("local");
 
@@ -24,4 +53,28 @@ test("files are being deleted upon user deleting their account", function() {
     $response->assertRedirect("/");
 
     Storage::disk("local")->assertMissing($user->name);
+});
+
+test("user is deleted", function() {
+    $user = User::factory()->create([
+        "password" => bcrypt("password")
+    ]);
+
+    $response = $this->actingAs($user)->delete("/settings", [
+        "password" => "password"
+    ]);
+
+    $response->assertRedirect("/");
+
+    $this->assertDatabaseMissing($user);
+});
+
+test("acccount deletion error while giving empty input", function() {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->delete("/settings", [
+        "password" => ""
+    ]);
+
+    $response->assertSessionHasErrors("password");
 });

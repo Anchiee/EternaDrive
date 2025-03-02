d1aeff064ab4c2ba2a4117b0425cf64ce1c66168
Anchie <wyhwtf@gmail.com>
2025-03-02 19:24:37 +0100

    test for delete files

--- tests/Feature/FileTest.php
+++ tests/Feature/FileTest.php
@@ -24,7 +24,7 @@ test("file uploading", function() {
     $this->assertDatabaseHas("files", [
         "name" => $file->name,
         "size" => $file->size * 1024,
-        "file_type" => $uploadedFile->extension(),
+        "file_type" => $uploadedFile->getMimeType(),
         "user_id" => $user->id
     ]);
 
@@ -35,4 +35,26 @@ test("guests cant enter the request", function() {
     $response = $this->post("/file");
 
     $response->assertRedirect("/login");
+});
+
+
+test("user can delete files", function() {
+    Storage::fake("local");
+
+    $user = User::factory()->create();
+    $file = File::factory()->create([
+        "user_id" => $user->id,
+        "random_name" => $user->name . "/example.png"
+    ]);
+
+    Storage::disk("local")->put($file->random_name, 
+    UploadedFile::fake()->create($file->name, $file->size, $file->file_type));
+
+    Storage::disk("local")->assertExists($file->random_name);
+
+    $this->actingAs($user)->delete(route("file.delete", ["file" => $file]));
+    
+    $this->assertDatabaseMissing("files", [
+        "id" => $file->id
+    ]);
 });

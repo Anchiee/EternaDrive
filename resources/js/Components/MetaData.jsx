f88bdb974f3f64b0bb3f197c963f00d5cddc22d6
Anchiee <wyhwtf@gmail.com>
2025-03-20 15:40:34 +0100

    lighthouse optimilizations

--- resources/js/Components/MetaData.jsx
+++ resources/js/Components/MetaData.jsx
@@ -6,6 +6,7 @@ export default function MetaData(props) {
         <Head>
             <title>{props.title}</title>
             <meta name="description" content={props.description}/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         </Head>
     )
 }

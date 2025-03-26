ea2b01320abe0179c8a3a82776811eaba47a699d
Anchiee <wyhwtf@gmail.com>
2025-03-26 21:33:04 +0100

    menu more options

--- resources/js/Components/Mobile/Dashboard.jsx
+++ resources/js/Components/Mobile/Dashboard.jsx
@@ -1,11 +1,13 @@
-import { usePage } from "@inertiajs/react"
 import useFiles from "@/Hooks/useFiles"
-import {SortableFilesContext} from "@/Contexts/Files"
-import { useContext } from "react"
+import { SortableFilesContext } from "@/Contexts/Files"
+import { useContext, useState } from "react"
+import { EllipsisVertical } from "lucide-react"
 
 export default function MobileDashboard() {
     const { sortableFiles } = useContext(SortableFilesContext)
     const {getFileIcon, getFileSize, getFormattedDate} = useFiles()
+
+    let [isShown, setIsShown] = useState({})
 
     return(
         <section className="grid grid-rows-3 grid-cols-2 gap-y-10 gap-x-5 mt-10 md:hidden">
@@ -15,14 +17,28 @@ export default function MobileDashboard() {
                     return(
                         <div key={index} className="bg-white-300 rounded-sm text-xs py-4">
 
-                            <div className="ml-4">
+                            <section className="ml-4 flex flex-col">
                                 <p className="flex items-center">
                                     {getFileIcon(file.file_type)}
-                                    {file.name}
+                                    <span className="break-all">{file.name}</span>
                                 </p>
                                 <p>{getFileSize(file.size)}</p>
                                 <p>{getFormattedDate(file.created_at)}</p>
-                            </div>
+
+                                <button 
+                                onClick={() => setIsShown(prev => ({...prev, [index]: !prev[index]}))} 
+                                className="self-end mr-4 relative">
+                                    <EllipsisVertical  size={17}/>
+
+                                    {isShown[index] &&
+                                        <section className="absolute bg-red h-10 w-10">
+                                        </section>
+                                    }
+                                </button>
+
+                                
+                                
+                            </section>
 
                         </div>
                     )

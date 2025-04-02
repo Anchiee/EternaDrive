86ecd005faa27568c7b448d2d6daf53883a405da
Anchiee <wyhwtf@gmail.com>
2025-04-02 23:30:56 +0200

    small changes

--- resources/js/Pages/Admin/Dashboard.jsx
+++ resources/js/Pages/Admin/Dashboard.jsx
@@ -36,7 +36,7 @@ export default function Dashboard() {
                 <Link  
                     as="button" 
                     href={route("admin.create")} 
-                    className="cursor-pointer text-white-300 py-1 rounded-md bg-red w-full opacity-90 transition-opacity" 
+                    className="cursor-pointer text-white-300 py-1 rounded-md bg-red w-full hover:opacity-90 transition-opacity" 
                     method="post">
 
                     Generate
@@ -53,7 +53,7 @@ export default function Dashboard() {
                                 <p className="flex items-center gap-2">
                                 {flash.adminName && <p className="text-xs">{flash.adminName}</p>}
                                 <button 
-                                className="bg-red px-2 py-1 text-xs text-white-300 rounded-sm cursor-pointer opacity-90 transition-opacity"
+                                className="bg-red px-2 py-1 text-xs text-white-300 rounded-sm cursor-pointer hover:opacity-90 transition-opacity"
                                 onClick={() => onCopyClick(flash.adminName)}
                                 >
                                     Copy
@@ -66,7 +66,7 @@ export default function Dashboard() {
                                 <p className="flex items-center gap-2">
                                     {flash.adminPass && <p className="text-[.8rem]">{flash.adminPass}</p>}
                                     <button 
-                                    className="bg-red px-2 py-1 text-xs text-white-300 rounded-sm cursor-pointer opacity-90 transition-opacity"
+                                    className="bg-red px-2 py-1 text-xs text-white-300 rounded-sm cursor-pointer hover:opacity-90 transition-opacity"
                                     onClick={() => onCopyClick(flash.adminPass)}
                                     >
                                         Copy
@@ -81,7 +81,7 @@ export default function Dashboard() {
                 
                 <div className="border-t-[1px] border-t-gray-400 my-4 py-4">
                     <Link as="button"
-                    className="block w-full bg-red py-1 rounded-sm text-white-300 hover:opacity-90 transition-opacity"
+                    className="block w-full bg-red py-1 rounded-sm text-white-300 hover:opacity-90 cursor-pointer transition-opacity"
                     href={route("admin.destroy")}
                     method="delete"
                     >

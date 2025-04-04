aa1502926c52329a2dccde826b1521d21c9cdcd7
Anchiee <wyhwtf@gmail.com>
2025-04-05 00:19:23 +0200

    more info for frontend

--- resources/js/Pages/Banned.jsx
+++ resources/js/Pages/Banned.jsx
@@ -1,12 +1,24 @@
 import CenteredCardLayout from "@/Layouts/CenteredCardLayout"
-
+import { usePage } from "@inertiajs/react"
 
 export default function Banned() {
+
+    const {auth} = usePage().props
+
+    const expirationDate = new Date(auth.user.ban_expires_at)
+    const expirationYear = expirationDate.getFullYear()
+    const expirationMonth = String(expirationDate.getMonth() + 1).padStart(2, "0")
+    const expirationDay = String(expirationDate.getDate()).padStart(2, "0")
+
+    const formattedDate = `${expirationDay}-${expirationMonth}-${expirationYear}`
+
+
     return(
         <CenteredCardLayout 
         title="Banned" 
         description="Unfortunately you have been banned. If it's a mistake by our side contact us immediately.">
-            <h1>You have been banned.</h1>
+            <h1 className="font-semibold">You have been banned.</h1>
+            <p>Expires at {formattedDate}</p>
             <p>For either more info or contact message us.</p>
         </CenteredCardLayout>
     )

3d635932ebccc454aa715710704a13c1067d3c59
Anchiee <wyhwtf@gmail.com>
2025-04-05 00:08:54 +0200

    expiration date for bans

--- app/Http/Middleware/Ban.php
+++ app/Http/Middleware/Ban.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-
+use Carbon\Carbon;
 
 class Ban
 {
@@ -17,8 +17,19 @@ class Ban
     public function handle(Request $request, Closure $next): Response
     {
         $user = auth()->user();
+        $todayDate = now();
 
         if($user && $user->is_banned) {
+
+            if($user->ban_expires_at && $user->ban_expires_at->lt($todayDate)) {
+                $user->is_banned = 0;
+                $user->ban_expires_at = null;
+
+                $user->save();
+
+                return $next($request);
+            }
+
             return redirect('/banned');
         }
         return $next($request);
